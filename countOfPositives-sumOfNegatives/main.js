7043cca7a899c98fe44d30234eb6c6469eaaf97c
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-08-15 22:48:32 -0600

    refactoring

--- countOfPositives-sumOfNegatives/main.js
+++ countOfPositives-sumOfNegatives/main.js
@@ -14,3 +14,6 @@ function countPositivesSumNegatives(input){
     }
     return [count, sum]
 }
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))//Should return [10,-65]
+
