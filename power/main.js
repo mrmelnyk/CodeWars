c2776193ffabe575144eeb628e79ce4778ad0b67
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-09-01 21:07:30 -0600

    multiply a number by itself a certain number of times

---
+++ power/main.js
@@ -0,0 +1,11 @@
+//The goal is to create a function of two inputs number and power, that "raises" the number up to power (ie multiplies number by itself power times).
+//Note: Math.pow and some other Math functions like eval() and ** are disabled.
+function numberToPower(number, power){
+    console.info(Math.log2(1024));
+    let x = 1
+    for (let i = 1; i <= power; i++){
+      x = x * number
+    }
+    return x 
+  }
+  
