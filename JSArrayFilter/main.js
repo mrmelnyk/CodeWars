c47875001bec3291fbf56327311917f851650c72
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-09-06 21:27:53 -0600

    filters out the odd numbers and returns the even

---
+++ JSArrayFilter/main.js
@@ -0,0 +1,5 @@
+//JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(num => num % 2 ==0)
+  }
