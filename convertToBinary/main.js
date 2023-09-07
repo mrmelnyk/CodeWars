dfedd23d892d15114be2177567fcf9d435015632
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-09-06 21:18:28 -0600

    convert an integer to binary

---
+++ convertToBinary/main.js
@@ -0,0 +1,7 @@
+function toBinary(n){
+    return Number(n.toString(2));
+  }
+
+  console.log(toBinary(5))//should log 101
+  console.log(toBinary(11))//should log 1011
+  console.log(toBinary(33))//should log 100001
