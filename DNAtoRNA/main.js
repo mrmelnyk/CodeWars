3be9ac815846e357f96197dbdcf1518163ed9ec9
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-09-17 21:24:31 -0600

    converts DNA string into RNA string

---
+++ DNAtoRNA/main.js
@@ -0,0 +1,11 @@
+//Create a function which translates a given DNA string into RNA.
+//The input string can be of arbitrary length - in particular, it may be empty. 
+//All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+    return dna.replace(/T/g, 'U')
+  }
+
+  console.log(DNAtoRNA('GACT'))//should log 'GACU'
+  console.log(DNAtoRNA('TGTACT'))//should log 'UGUACU'
+  
