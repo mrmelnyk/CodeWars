f33fe4f9b3bccf280305bfc36ec33808dccc1c09
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-09-22 22:07:09 -0600

    return the ascii value from the given character

---
+++ getASCIIFromChar/main.js
@@ -0,0 +1,8 @@
+//Get ASCII value of a character.
+
+function getASCII(c){
+    return c.charCodeAt(0)
+  }
+
+  console.log(getASCII('A'))//should log 65
+  console.log(getASCII('!'))//should log 33
