b0c2984a7d9bfbabe86b41d1e2d109eb92623c92
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-09-26 21:23:53 -0600

    return how many digits are in an integer

---
+++ numOfDigits/main.js
@@ -0,0 +1,11 @@
+//Determine the total number of digits in the integer (n>=0) given as input to the function. 
+//For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+//All inputs will be valid.
+
+function digits(n) {
+    let str = n.toString()
+    return str.length
+  }
+
+  console.log(digits(123))//should log 3
+  console.log(digits(456789))//should log 6
