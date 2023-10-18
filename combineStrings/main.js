e71f0ed9a4caef8cb640d5e8013e5507c9ebca79
mrmelnyk.dev@gmail.com <mrmelnyk.dev@gmail.com>
2023-10-18 10:05:42 -0600

    write a function that combines two strings

---
+++ combineStrings/main.js
@@ -0,0 +1,7 @@
+//write a function that combines two strinngs into one.
+
+function combineNames(a,b){
+  return `${a} ${b}`;
+}
+
+console.log(combineNames('Andrew', 'Melnyk'))//should log 'Andrew Melnyk'
