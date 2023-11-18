9bd9df60c293aec0838c0e5da7b1bb116449a09d
mrmelnyk.dev@gmail.com <mrmelnyk.dev@gmail.com>
2023-11-18 10:21:26 -0700

    given two strings concat them with an are in the middle using template literal

---
+++ templateStrings/main.js
@@ -0,0 +1,7 @@
+//given two strings, use the template literal format to concatenate the two strings with an 'are' in the middle.
+
+function templateStrings(ob, feat){
+  return `${obj} are ${feat}`;
+}
+
+console.log(templateStrings('Dogs', 'fun'))//should log 'Dogs are fun'
