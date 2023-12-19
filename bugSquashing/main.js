9eb9a2048817ac11b8515aefa6b48ce0216217a6
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-12-18 20:54:13 -0700

    Create a function call main that calls the functions in the proper order.

---
+++ bugSquashing/main.js
@@ -0,0 +1,17 @@
+//You are creating a text-based terminal version of your favorite board game.
+//In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+//Create a function named main that calls the functions in the proper order.
+
+let health = 100;
+let position = 0;
+let coins = 0;
+
+function main() {
+  rollDice();
+  move() ;
+  combat();
+  getCoins();
+  buyHealth();
+  printStatus();
+}
+
