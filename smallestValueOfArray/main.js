6b178d8fb0bdb414149fc4dc504a7e763fe1a86d
MrMelnyk <mrmelnyk.dev@gmail.com>
2023-12-23 23:13:38 -0700

    Write a function that can return the smallest value of an array or the index of that value.

---
+++ smallestValueOfArray/main.js
@@ -0,0 +1,11 @@
+//Write a function that can return the smallest value of an array or the index of that value. 
+//The function's 2nd parameter will tell whether it should return the value or the index.
+//Assume the first parameter will always be an array filled with at least 1 number and no duplicates. 
+//Assume the second parameter will be a string holding one of two values: 'value' and 'index'.
+
+function min(arr, toReturn) {
+    return toReturn == 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
+}
+
+console.log(min([23,29,46,2,3,67], 'value'))//should log 2
+console.log(min([23,29,46,2,3,67], 'index'))//should log 3
