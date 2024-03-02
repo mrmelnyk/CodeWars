ff83c298e8c5ab7a5f895c69231c15328d3f040e
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-03-01 21:04:38 -0700

    Write a function that returns the elements of an array sorted in lexigraphicl order.

---
+++ lexigraphicalSort/main.js
@@ -0,0 +1,8 @@
+//Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+function sortme(names){
+    return names.sort();
+  }
+
+  console.log(sortme(['one','two','three','four']))//should return ['four', 'one','three','two']
+  
