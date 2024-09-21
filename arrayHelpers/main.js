644c4903960a8c307373fd5e5b85f1f9d19dc63b
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-09-21 08:39:22 -0600

    Extend the built in Array class to include the given methods

---
+++ arrayHelpers/main.js
@@ -0,0 +1,27 @@
+/*
+Extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+*/
+
+Array.prototype.square = function() {
+    return this.map(x => x*x);
+  }
+  
+  Array.prototype.cube = function() {
+    return this.map(x => x*x*x);
+  }
+  
+  Array.prototype.average = function() {
+    return this.reduce((a,b) => a+b)/this.length;
+  }
+  
+  Array.prototype.sum = function() {
+    return this.reduce((a,b) => a+b);
+  }
+  
+  Array.prototype.even = function() {
+    return this.filter(x => !(x % 2));
+  }
+  
+  Array.prototype.odd = function (){
+    return this.filter(x => x % 2);
+  }
