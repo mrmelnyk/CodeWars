d632fdd571d72f5feecd1db5500a03503363b34d
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-10-10 09:47:58 -0600

    Write a function that returns the minimal and maximal values of an array, in that order.

---
+++ findMinAndMax/main.js
@@ -0,0 +1,8 @@
+//Write a function that returns the minimal and maximal value of a list (in that order).
+
+function getMinMax(arr){
+    let srt =arr.sort((a,b) => a-b);
+    return new Array(srt[0], srt[srt.length-1]);
+};
+
+console.log(getMinMax([4,1,6,3,4,8,2]))//should log [1,8]
