b918512ef367c6c0828c4fab31195359756451c8
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-10-10 09:49:45 -0600

    Fixed a little syntax error.

--- findMinAndMax/main.js
+++ findMinAndMax/main.js
@@ -1,7 +1,7 @@
 //Write a function that returns the minimal and maximal value of a list (in that order).
 
 function getMinMax(arr){
-    let srt =arr.sort((a,b) => a-b);
+    let srt = arr.sort((a,b) => a-b);
     return new Array(srt[0], srt[srt.length-1]);
 };
 
