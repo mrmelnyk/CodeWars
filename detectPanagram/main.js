78e2e5e95684c771c93026c899a220175a7776da
MrMelnyk <mrmelnyk.dev@gmail.com>
2024-11-13 20:55:30 -0700

    Given a string, detect whether or not it is a panagram. Return true if it is, else return false.

---
+++ detectPanagram/main.js
@@ -0,0 +1,13 @@
+//Given a string, detect whether or not it is a paragram. Return true if it is, else return false.
+
+function detectPanagram(str){
+    let lc = str.toLowerCase();
+    for(let char of 'abcdefghijklmnopqrstuvwxyz'){
+        if(!lc.includes(char)){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(detectPanagram('The quick fox jumps over the lazy brown dog'))//should log true
